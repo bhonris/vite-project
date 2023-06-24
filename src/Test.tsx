f1bebe287d17959bc6545b40f2db887301d906e8
Bhon Bunnag <bhonris@gmail.com>
2023-06-24 00:03:33 -0700

    add problem

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,45 +1,23 @@
-import { useState } from "react";
-import { useVegetables } from "./useVegetable";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useEffect } from "react";
 
-export interface IVegetables {
-  name: string;
-  price: number;
-}
+const Home = () => {
+  const [data, setData] = useState<any[]>([]);
 
-const Maintenance = () => {
-  const [limit, setLimit] = useState(50);
-  const { vegetable, loading, error } = useVegetables(limit);
-
-  const vegetableList = vegetable.map((vegtable) => (
-    <li key={vegtable.name}>
-      {vegtable.name}: {vegtable.price}
-    </li>
-  ));
-
-  const limitPicker = () => {
-    return (
-      <select
-        value={limit}
-        onChange={(e) => setLimit(Number(e.target.value))}
-        name="limit"
-        id="limit"
-      >
-        <option value="10">10</option>
-        <option value="20">20</option>
-        <option value="30">30</option>
-        <option value="40">40</option>
-        <option value="50">50</option>
-      </select>
-    );
-  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
   return (
-    <div>
-      <div>{limitPicker()}</div>
-      {error !== null && <div>An error was thrown</div>}
-      {loading ? <div>Loading...</div> : <div>{vegetableList}</div>}
-    </div>
+    <>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </>
   );
 };
 
-export default Maintenance;
+export default Home;
